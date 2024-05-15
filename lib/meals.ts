1f60d18848cc5a2e1030ee997d7180992de63196
rajvpatil5 <48220124+rajvpatil5@users.noreply.github.com>
2024-05-15 10:26:46 +0530

    remove waiting time promise

--- lib/meals.ts
+++ lib/meals.ts
@@ -2,7 +2,7 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export const getMeals = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
   // throw new Error("Loading meals failed");
   return db.prepare("select * from meals").all();
 };
